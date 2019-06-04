ad32d5e5951eadc2088535f6a52cc8cba0dd3b54
Carola <carola.bothe@gmx.de>
2019-06-04 11:20:12 +0200

    redid ertefact cleaning

---
+++ runall_readeeg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+i=1
+end=21
+while (($i <= $end)); do
+    echo python -W ignore read_eeg.py Data/ $i Results/
+    python -W ignore read_eeg.py Data/ $i Results/
+    wait
+    i=$(($i + 1))
+done
