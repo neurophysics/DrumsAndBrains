22e79cec1e9bcc4c89cb9121725096cf55a12425
Carola <carola.bothe@gmx.de>
2020-10-19 14:20:24 +0200

    plot update

--- bestDipoleFit.py
+++ bestDipoleFit.py
@@ -89,17 +89,27 @@ snare_singleMRI = mni2mri(snare_singleMNI)
 snare_pairedMRI = np.array([mni2mri(snare_pairedMNI[0]),mni2mri(snare_pairedMNI[1])])
 
 mri = mri.swapaxes(0,2) #X should be left to right, Y back front, Z down up
-
+#mri.shape = (394, 466, 378)
 l = snare_pairedMRI[0].astype(int)
+#l = (l[0],l[2],l[1]) #swap axes as in mri
 r = snare_pairedMRI[1].astype(int)
-fig,ax = plt.subplots(3,1)
-ax[0].imshow(mri[67,:,:].T)
+#r = (r[0],r[2],r[1]) #swap axes as in mri
+
+fig,ax = plt.subplots(3,1,figsize=(4,10))
+plt.subplots_adjust(hspace=0.4)
+ax[0].imshow(mri[l[0],:,:].T) #lim: 466x378
+#ax[0].scatter(r[1],r[2],s=5, c='orange')
+ax[0].set_title('Sagittal')
+ax[0].invert_yaxis() #otherwise the picture is turned upside down
 ax[0].scatter(l[1],l[2],s=5, c='red')
-ax[0].scatter(r[1],r[2],s=5, c='orange')
-ax[1].imshow(mri[:,163,:].T)
-ax[1].scatter(l[0],l[2],s=5, c='red')
-ax[1].scatter(r[0],r[2],s=5, c='orange')
-ax[2].imshow(mri[:,:,217].T)
-ax[2].scatter(l[0],l[1],s=5, c='red')
-ax[2].scatter(r[0],r[1],s=5, c='orange')
+ax[1].imshow(mri[:,l[1],:].T) #lim: 394x378
+ax[1].invert_yaxis()
+ax[1].scatter(l[0],l[1],s=5, c='red')
+ax[1].scatter(r[0],r[1],s=5, c='orange')
+ax[1].set_title('Coronal')
+ax[2].imshow(mri[:,:,l[2]].T) #lim: 394x466
+ax[2].invert_yaxis()
+ax[2].scatter(l[0],l[2],s=5, c='red')
+ax[2].scatter(r[0],r[2],s=5, c='orange')
+ax[2].set_title('Horizontal')
 plt.show()
