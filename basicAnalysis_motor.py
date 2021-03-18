e3103dc15e73f559901fb573dcdbd623a5f8af61
carolabothe <carola.bothe@gmx.de>
2021-03-18 15:04:53 +0100

    added subject average

--- basicAnalysis_motor.py
+++ basicAnalysis_motor.py
@@ -8,7 +8,7 @@ import scipy as sp
 
 data_folder = sys.argv[1]
 result_folder = sys.argv[2]
-N_subjects = 21 #21 later, for now bc of speed
+N_subjects = 3 #21 later, for now bc of speed (10min per subject)
 s_rate = 1000 # sampling rate of the EEG
 
 # target frequencies
@@ -24,9 +24,14 @@ chancoords_2d = meet.sphere.projectSphereOnCircle(chancoords,
         projection='stereographic')
 
 ##### plot 2 sec preresponse for each channel #####
+all_BP = [] #avg over all subjects
+all_ERD = [] #avg over all subjects
+fbands = [[1,4], [4,8], [8,12], [12,20], [20,40]]
+win = [-2000, 500]
 idx = 0 #index to asses eeg (0 to 19)
 subj = 1 #index for subject number (1 to 10, 12 to 21)
 while(subj <= N_subjects):
+    print(subj)
     # skip subject without eeg data
     if not os.path.exists(os.path.join(
         result_folder, 'S{:02d}'.format(subj), 'prepared_FFTSSD.npz')):
@@ -58,8 +63,8 @@ while(subj <= N_subjects):
         snareCue_DevToClock = f['snareCue_DevToClock']
         wdBlkCue_nearestClock = f['wdBlkCue_nearestClock']
         wdBlkCue_DevToClock = f['wdBlkCue_DevToClock']
-        snareCue_times = f['snareCue_times']
-        wdBlkCue_times = f['wdBlkCue_times']
+        #snareCue_times = f['snareCue_times']
+        #wdBlkCue_times = f['wdBlkCue_times']
         bar_duration = f['bar_duration']
         snare_deviation = f['snare_deviation']
         wdBlk_deviation = f['wdBlk_deviation']
@@ -78,7 +83,6 @@ while(subj <= N_subjects):
     wdBlk_resp = wdBlk_resp[~wdBlk_resp_outlier].astype(int)
 
     #plot 2000ms pre response for each channel
-    win = [-2000, 500]
     snareInlier = np.all(meet.epochEEG(artifact_mask, snare_resp,
         win), 0)
     wdBlkInlier = np.all(meet.epochEEG(artifact_mask, wdBlk_resp,
@@ -92,52 +96,92 @@ while(subj <= N_subjects):
     fig, axs = plt.subplots(int(np.ceil(Nc/4)), 4, figsize=(8,12),
             sharex=True, sharey=True)
     fig.subplots_adjust(top=0.95, bottom=0.05)
-    axs[0,0].set_title('2000 ms preresponse')
-    axs[0,1].set_title('2000 ms preresponse')
-    axs[0,2].set_title('2000 ms preresponse')
-    axs[0,3].set_title('2000 ms preresponse')
+    fig.suptitle('BP: 2000 ms preresponse')
     for c in range(Nc):
-        axs[c//4, c%4].plot(range(*win), all_trials.mean(-1)[c])
+        axs[c//4, c%4].plot(range(*win), all_trials.mean(-1)[c], linewidth=1)
         axs[c//4, c%4].set_ylabel(channames[c], fontsize=8)
         axs[c//4, c%4].set_yticks([])
         axs[c//4, c%4].axvline(0, lw=0.5, c='k')
     #plt.show()
     fig.savefig(os.path.join(save_folder, 'motor_BP_2000mspreresponse'))
+    all_BP.append(all_trials.mean(-1))
 
     # ERD in frequency bands 1-4, 4-8, 8-12, 12-20, 20-40
-    fbands = [[1,4], [4,8], [8,12], [12,20], [20,40]]
-    i=3 #later loop over all frequency bands
+    ERDs = []
+    for band in fbands:#later loop over all frequency bands
     # 1. band-pass filters with order 6 (3 into each direction)
-    Wn = np.array(fbands[i]) / s_rate * 2
-    b, a = sp.signal.butter(3, Wn, btype='bandpass')
-    eeg_filt = sp.signal.filtfilt(b, a, eeg)
-    #2. Hilbert-Transform, absolute value
-    eeg_filtHil = np.abs(sp.signal.hilbert(eeg_filt, axis=-1))
-    #3. Normalisieren, so dass 2 sek prä-stimulus 100% sind und dann averagen
-    all_trials_filt = meet.epochEEG(eeg_filtHil,
-            np.r_[snare_resp[snareInlier],
-                wdBlk_resp[wdBlkInlier]],
-            win)
-    ERD = all_trials_filt.mean(-1)
-    ERD /= ERD[:,0][:,np.newaxis]
-    ERD *= 100
-    fig, axs = plt.subplots(int(np.ceil(Nc/4)), 4, figsize=(8,12),
+        Wn = np.array(band) / s_rate * 2
+        b, a = sp.signal.butter(3, Wn, btype='bandpass')
+        eeg_filt = sp.signal.filtfilt(b, a, eeg)
+        #2. Hilbert-Transform, absolute value
+        eeg_filtHil = np.abs(sp.signal.hilbert(eeg_filt, axis=-1))
+        #3. Normalisieren, so dass 2 sek prä-stimulus 100% sind und dann averagen
+        all_trials_filt = meet.epochEEG(eeg_filtHil,
+                np.r_[snare_resp[snareInlier],
+                    wdBlk_resp[wdBlkInlier]],
+                win)
+        ERD = all_trials_filt.mean(-1)
+        ERD /= ERD[:,0][:,np.newaxis]
+        ERD *= 100
+        ERDs.append(ERD)
+    fig, axs = plt.subplots(int(np.ceil(Nc/3)), 3, figsize=(7,7),
             sharex=True, sharey=True)
     fig.subplots_adjust(top=0.95, bottom=0.05)
-    axs[0,0].set_title('2000 ms preresponse')
-    axs[0,1].set_title('2000 ms preresponse')
-    axs[0,2].set_title('2000 ms preresponse')
-    axs[0,3].set_title('2000 ms preresponse')
+    fig.suptitle('ERD: 2000 ms preresponse')
     for c in range(Nc):
-        axs[c//4, c%4].plot(range(*win), ERD[c])
-        axs[c//4, c%4].set_ylabel(channames[c], fontsize=8)
-        #if c%4 != 0:
-        #    axs[c//4, c%4].set_yticks([])
-        axs[c//4, c%4].axvline(0, lw=0.5, c='k')
-        axs[c//4, c%4].axhline(100, lw=0.5, c='r', ls=':')
+        handels = [] # for legend
+        for i,ERD in enumerate(ERDs):
+            h, = axs[c//3, c%3].plot(range(*win), ERD[c], linewidth=1)
+            handels.append(h)
+        axs[c//3, c%3].set_ylabel(channames[c], fontsize=8)
+        axs[c//3, c%3].axvline(0, lw=0.5, c='k')
+        axs[c//3, c%3].axhline(100, lw=0.5, c='r', ls=':')
+    fig.delaxes(axs[c//3, 2])
+    plt.legend(handels,['frequency band '+str(i[0])+'-'+str(i[1]) +' Hz'
+        for i in fbands], bbox_to_anchor=(1.7, 1.2), loc='upper center',
+        borderaxespad=1, fontsize=5)
     #plt.show()
     fig.savefig(os.path.join(save_folder, 'motor_ERD_2000mspreresponse'))
+    all_ERD.append(ERDs)
     idx += 1
     subj += 1
+    plt.close('all')
 
-plt.close('all')
+1/0
+# plot BP for all subjects
+all_BP_avg = np.mean(all_BP, axis=0)
+fig, axs = plt.subplots(int(np.ceil(Nc/4)), 4, figsize=(8,7),
+        sharex=True, sharey=True)
+fig.tight_layout()
+fig.subplots_adjust(top=0.95, bottom=0.05)
+fig.suptitle('BP: 2000 ms preresponse, subject average')
+for c in range(Nc):
+    axs[c//4, c%4].plot(range(*win), all_BP_avg[c], linewidth=1)
+    axs[c//4, c%4].set_ylabel(channames[c], fontsize=8)
+    axs[c//4, c%4].set_yticks([])
+    axs[c//4, c%4].axvline(0, lw=0.5, c='k')
+#plt.show()
+fig.savefig(os.path.join(result_folder, 'motor_BP_2000mspreresponse'))
+
+# plot ERD for all subjects
+all_ERD_avg = [ np.mean([i[j] for i in all_ERD], axis=0)
+    for j in range(len(fbands))] # for each band, average over subjects
+#[for i in all_ERD]
+fig, axs = plt.subplots(int(np.ceil(Nc/3)), 3, figsize=(7,7),
+        sharex=True, sharey=True)
+fig.subplots_adjust(top=0.95, bottom=0.05)
+fig.suptitle('ERD: 2000 ms preresponse')
+for c in range(Nc):
+    handels = [] # for legend
+    for i,ERD in enumerate(all_ERD_avg):
+        h, = axs[c//3, c%3].plot(range(*win), ERD[c], linewidth=1)
+        handels.append(h)
+    axs[c//3, c%3].set_ylabel(channames[c], fontsize=8)
+    axs[c//3, c%3].axvline(0, lw=0.5, c='k')
+    axs[c//3, c%3].axhline(100, lw=0.5, c='r', ls=':')
+fig.delaxes(axs[c//3, 2])
+plt.legend(handels,['frequency band '+str(i[0])+'-'+str(i[1]) +' Hz'
+    for i in fbands], bbox_to_anchor=(1.7, 1.2), loc='upper center',
+    borderaxespad=1, fontsize=5)
+#plt.show()
+fig.savefig(os.path.join(save_folder, 'motor_ERD_2000mspreresponse'))
