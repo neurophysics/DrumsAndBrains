6a2ff9d40d5af8b3c4736971bc1430fdbaede08f
carolabothe <carola.bothe@gmx.de>
2021-05-27 13:24:33 +0200

    calculates CSP

--- csp.py
+++ csp.py
@@ -3,19 +3,16 @@ executes common spatial pattern algorithm on ERD data
 """
 import numpy as np
 import sys
-import csv
 import os.path
 import matplotlib.pyplot as plt
+import scipy
 import meet
-import helper_functions
-import scipy as sp
 
 data_folder = sys.argv[1]
 result_folder = sys.argv[2]
 N_subjects = 21
 s_rate = 1000 # sampling rate of the EEG
 fbands = [[1,4], [4,8], [8,12], [12,20], [20,40]]
-
 
 # read ERD data
 # erd data is referenced to the average eeg amplitude, bandpass filtered
@@ -37,17 +34,46 @@ while True:
     except KeyError:
         break
 
-1/0
-#from AAND exercise 5 solution:
-def train_CSP(epo, mrk_class):
-# Usage: W, D = trainCSP(epo, mrk_class)
-    C = epo.shape[1]
-    X1 = np.reshape(np.transpose(epo[:,:,mrk_class==0], (1,0,2)), (C, -1))
-    S1 = np.cov(X1)
-    X2 = np.reshape(np.transpose(epo[:,:,mrk_class==1], (1,0,2)), (C, -1))
-    S2 = np.cov(X2)
-    D, W = scipy.linalg.eigh(a=S1, b=S1+S2)
-    return W, D # D are EV, W is spatial pattern matrix
+# only take alpha (8-12) band for Now
+ERD_alpha = [e[2] for e in ERD]
+
+# calculate covariance matrices for all subjects
+contrast_cov = []
+target_cov = []
+for erd in ERD_alpha:
+    X_baseline = erd[:, :900] # class 1: baseline activity (on plot seemed to be -2 to -1.1s)
+    X_erd = erd[:, 900:2000] # class 2: pre-response activity (ERD)
+    contrast_cov.append(np.cov(X_baseline))
+    target_cov.append(np.cov(X_erd))
+
+## average the covariance matrices across all subjects
+#@gunnar: we did this for ssd, here as well? would make sense to me
+for t, c in zip(target_cov, contrast_cov):
+    # normalize by the trace of the contrast covariance matrix
+    t_now = t/np.trace(c)
+    c_now = c/np.trace(c)
+    try:
+        all_target_cov += t_now
+        all_contrast_cov += c_now
+    except: #init
+        all_target_cov = t_now
+        all_contrast_cov = c_now
+
+# calculate CSP
+## EV and filter
+#@gunnar: should I use helper_function.eigh_rank as you did for ssd?
+CSP_eigvals, CSP_filters = scipy.linalg.eigh(all_target_cov,
+        all_target_cov + all_contrast_cov)
+
+# patterns
+CSP_patterns = scipy.linalg.solve(
+        CSP_filters.T.dot(all_target_cov).dot(CSP_filters),
+        CSP_filters.T.dot(all_target_cov))
+
+### normalize the patterns such that Cz is always positive
+#@gunnar: we did this for ssd, here as well? would make sense to me
+channames = meet.sphere.getChannelNames('channels.txt')
+CSP_patterns*=np.sign(CSP_patterns[:,np.asarray(channames)=='CZ'])
 
 
 np.savez(os.path.join(result_folder, 'CSP.npz'),
