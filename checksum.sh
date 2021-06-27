1eb5737a9bf0a2635691ed3bcb306bd6a8363185
carolabothe <carola.bothe@gmx.de>
2021-06-27 13:17:06 +0200

    added modification date and file loop

--- checksum.sh
+++ checksum.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# print md5 checksum and modification date for files
 i=1
 end=9
 cd Data/S01
 while (($i <= $end)); do
     cd ../S0$i
     echo S0$i
-    md5 clean_data.npz
-    md5 ICA_result.joblib
-    md5 artifact_segments.npy
-    md5 S0$i\_eeg.eeg
-    md5 reject_ICs.txt
+    files=('clean_data.npz' 'ICA_result.joblib' 'artifact_segments.npy' 'S0$i\_eeg.eeg' 'reject_ICs.txt')
+    for file in "${files[@]}"
+    do
+        m=$(md5 $file | awk '{ print $4 }')
+        l=$(ls -l $file | awk '{ print $6 $7 $8 }')
+        echo $m $l $file
+    done
     echo
     wait
     i=$(($i + 1))
@@ -19,12 +22,14 @@ i=10
 end=21
 while (($i <= $end)); do
     cd ../S$i
-    echo S$i 
-    md5 clean_data.npz
-    md5 ICA_result.joblib
-    md5 artifact_segments.npy
-    md5 S$i\_eeg.eeg
-    md5 reject_ICs.txt
+    echo S$i
+    files=('clean_data.npz' 'ICA_result.joblib' 'artifact_segments.npy' 'S0$i\_eeg.eeg' 'reject_ICs.txt')
+    for file in "${files[@]}"
+    do
+        m=$(md5 $file | awk '{ print $4 }')
+        l=$(ls -l $file | awk '{ print $6 $7 $8 }')
+        echo $m $l $file
+    done
     echo
     wait
     i=$(($i + 1))
