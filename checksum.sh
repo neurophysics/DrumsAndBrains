91d01a58cb095474680c356f0d672d67e0fce3a7
Gunnar Waterstraat <gunnar.watestraat@charite.de>
2021-06-30 12:39:07 +0200

    Synchronized all source files

--- checksum.sh
+++ checksum.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 # print md5 checksum and modification date for files
+set -u
 i=1
-end=9
-cd Data/S01
-while (($i <= $end)); do
-    cd ../S0$i
-    echo S0$i
-    files=('clean_data.npz' 'ICA_result.joblib' 'artifact_segments.npy' 'S0$i\_eeg.eeg' 'reject_ICs.txt')
+end=0
+cd ~/Neuro/Data/2018-Polyrhythm/S01
+while ((${i} <= ${end})); do
+    cd ../S0${i}
+    echo S0${i}
+    files=('clean_data.npz' 'ICA_result.joblib' 'artifact_segments.npy' "S0${i}_eeg.eeg" 'reject_ICs.txt')
     for file in "${files[@]}"
     do
-        m=$(md5 $file | awk '{ print $4 }')
+        #m=$(md5sum $file | awk '{ print $4 }')
         l=$(ls -l $file | awk '{ print $6 $7 $8 }')
-        echo $m $l $file
+	echo $(md5sum $file)
+        echo $l
+	echo ''
     done
     echo
     wait
     i=$(($i + 1))
 done
 
-i=10
+i=21
 end=21
-while (($i <= $end)); do
-    cd ../S$i
-    echo S$i
-    files=('clean_data.npz' 'ICA_result.joblib' 'artifact_segments.npy' 'S0$i\_eeg.eeg' 'reject_ICs.txt')
+cd ~/Neuro/Data/2018-Polyrhythm/S01
+while ((${i} <= ${end})); do
+    cd ../S${i}
+    echo S${i}
+    files=('clean_data.npz' 'ICA_result.joblib' 'artifact_segments.npy' "S${i}_eeg.eeg" 'reject_ICs.txt')
     for file in "${files[@]}"
     do
-        m=$(md5 $file | awk '{ print $4 }')
+        #m=$(md5sum $file | awk '{ print $4 }')
         l=$(ls -l $file | awk '{ print $6 $7 $8 }')
-        echo $m $l $file
+	echo $(md5sum $file)
+        echo $l
+	echo ''
     done
     echo
     wait
