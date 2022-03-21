21fd812be0aebb5217ad92b6f0fd35987f565213
Gunnar Waterstraat <gunnar.watestraat@charite.de>
2022-03-21 15:11:15 +0100

    added LMMELSM mode

---
+++ lmmelsm.R
@@ -0,0 +1,16 @@
+### test brms package with our data
+# see Williams et al. 2019 paper
+
+# todo:
+# skripte für daten durchlaufen lassen
+library('LMMELSM')
+
+snare_dat <- read.csv("Results/snare_data_silence.csv", sep=',')
+
+fit <- lmmelsm(
+	       list(
+	observed ~ deviation,
+	location ~ musicality + trial + session + Snare1_between + Snare2_between + Snare3_between + Snare1_within + Snare2_within + Snare3_within,
+	scale ~ musicality + trial + session + Snare1_between + Snare2_between + Snare3_between + Snare1_within + Snare2_within + Snare3_within,
+	between ~ musicality + trial + session + Snare1_between + Snare2_between + Snare3_between + Snare1_within + Snare2_within + Snare3_within),
+	group = subject, snare_dat, cores=8)
