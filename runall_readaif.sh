f935e7cbd3ac063d60f9d769583534c7c6b20119
carolabothe <carola.bothe@gmx.de>
2023-07-24 15:39:19 +0200

    .gitignore is ignoring BP.npz

--- runall_readaif.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-i=1
-end=21
-while (($i <= $end)); do
-    python3 read_aif.py Data/ $i Results/ #~/Neuro/Data/2018-Polyrhythm
-    echo python3 read_aif.py Data $i Results/
-    wait
-    i=$(($i + 1))
-done
--- runall_readeeg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-i=1
-end=21
-while (($i <= $end)); do
-    echo python3 -W ignore read_eeg.py Data/ $i Results/
-    python3 -W ignore read_eeg.py Data/ $i Results/
-    wait
-    i=$(($i + 1))
-done
