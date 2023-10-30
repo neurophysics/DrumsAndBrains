90e2267f3eb9db87f924ad91dc5211cdaedbafbd
carolabothe <carola.bothe@gmx.de>
2023-10-30 14:50:07 +0100

    visualization for snare so far

--- lmmelsm_plotCI.py
+++ lmmelsm_plotCI.py
@@ -1,93 +1,118 @@
-#Der Plot zeigt das Konfidenzintervall und das spuckt LMMELSM ja auch direkt aus. D.h., in jeder Zeile ist der 2.5-97.5 %-Wert geplottet. Zusätzlich habe ich den p-value darüber geschrieben - den könnte man aber weglassen, um Platz zu sparen.
+# questions for gunnar
+# ? should we add an averaged intercept or sth as we want to make a point with earlier and later?
+# how about we divide into snare and wdblk for overview? im gonna do snare for now
 
-#Ich würde vorschlagen, wir machen 2 Plots. Einen für Location mit der Beschrfitung earlier <-> later und dann alle Linien für sowohl snare als auch woodblock parameter (wenn man die p-Werte weglässt passen ja vllt. sogar die between und within Parameter rauf). Und einen für Scale mit der Achsenbeschriftung consistent performance <-> irregular performance. Dann hätte jeder Plot, glaube ich 11 Linien. Wenn das zu unübersichtlich wird, dann machen wir nur between? Und dann lass uns hier nur die Parameter der univariaten Modelle auf diese Weise plotten - wir hatten mit Gabriel ja besprochen, dass dies unser Schwerpunkt sein sollte.
-
-#todo
-# test with other files
-# wenn nötig noch den zugehörigen p-wert aus verwandten file (error wenn nicht da)
 
 import numpy as np
+import scipy
+from scipy import stats
 import sys
 import os.path
 import re
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-model_name = sys.argv[1] #e.g. snare_musicality
-result_folder = sys.argv[2]
-file_path = result_folder + 'models/singleVariable/'+model_name+'.txt'
+result_folder = sys.argv[1]
 
-# Open and read the file
-with open(file_path, 'r') as file:
-    intercept_lines = []
-    variable_lines = []
 
-    for line in file:
-        if re.search(r'\s+(\w+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)',  line): #thanks, chatGPT (match lines in the format "label number number," where "label" is one or more word characters, and "number" can be a positive or negative decimal number with one or more digits before and after the decimal point)
-            if "deviation" in line:
-                intercept_lines.append(line.strip())
-            else:
-                variable_lines.append(line.strip())
+location_dict = {} ##should have:
+# snare trial
+# snare session
+# snare musicality
+# snare EEG1 within
+# snare EEG2 within
+# snare EEG1 between
+# snare EEG2 between
+# and these 7 again for wdblk so 14 overall
+# only do snare for now...
+scale_dict = {} ##should have:
 
-#e.g. deviation 0.0791 0.0787 0.021 0.0383 0.121 where order is  param  predictor   Mean Median    SD   Q2.5   Q97.5
-loc_intercept = intercept_lines[0]
-scale_intercept = intercept_lines[1]
-loc_RE = intercept_lines[2]
-scale_RE = intercept_lines[3]
+names = ['trial', 'session', 'musicality','EEG1_within', 'EEG2_within','EEG1_between','EEG2_between']
+name_i = 0
 
-loc_variable = variable_lines[0]
-scale_variable = variable_lines[1]
-loc_group = variable_lines[2]
-scale_group = variable_lines[3]
+all_files = [result_folder + 'models/singleVariable/' + name +'.txt'
+             for name in [
+                     'snare_trial0995',
+                     'snare_session',
+                     'snare_musicality0995',
+                     'snare_Snare1_within',
+                     'snare_Snare2_within',
+                     'snare_Snare1_between',
+                     'snare_Snare2_between']]
 
-1/0
+#loop over all single variabkle model files
+for file_path in all_files:
+        # Open and read the file
+        with open(file_path, 'r') as file:
+                intercept_lines = []
+                variable_lines = []
+                for line in file:
+                        if re.search(r'\s+(\w+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)',  line): #thanks, chatGPT (match lines in the format "label number number," where "label" is one or more word characters, and "number" can be a positive or negative decimal number with one or more digits before and after the decimal point)
+                                if "deviation" in line:
+                                        intercept_lines.append(line.strip())
+                                else:
+                                        variable_lines.append(line.strip())
+
+                #e.g. deviation 0.0791 0.0787 0.021 0.0383 0.121 where order is  param  predictor   Mean Median    SD   Q2.5   Q97.5 so last five are numbers
+                data_dict = {}
+                name_list = ['loc_intercept', 'scale_intercept', 'loc_RE','scale_RE']
+                for c,i in enumerate(intercept_lines):
+                        parts = i.split()
+                        key = name_list[c]
+                        values = [float(value) for value in parts[-5:]]
+                        data_dict[key] = values
+                name_list_var = ['loc_variable', 'scale_variable', 'loc_group','scale_group'] #order in teh files never changes
+                for c,i in enumerate(variable_lines):
+                        parts = i.split()
+                        key = name_list_var[c]
+                        values = [float(value) for value in parts[-5:]]
+                        data_dict[key] = values
+
+                #we only need the one location and scale CIs for now
+                key = names[name_i]
+                location_dict[key] = data_dict['loc_variable']
+                scale_dict[key] = data_dict['scale_variable']
+                name_i+=1
+
+
+#params_p=np.arange(0.1,0.8,0.1) #CHANGE LATER by reading from _p file
+
+#final data should have shape (number of variables, 2) 2 bc its an interval
+
+############# i gotta check this!!!!!!
+params_CI_loc = np.vstack(
+                [value[-2:] for key, value in location_dict.items()]
+                )
+
+params_CI_scale = np.vstack(
+                [value[-2:] for key, value in scale_dict.items()]
+                )
+
+
 ###########################################
 # plot the bootstrap confidence intervals #
 ###########################################
-params_CI = np.vstack([
-    scipy.stats.scoreatpercentile(large_bootstrap_params[:,t==0], 2.5,
-        axis=0),
-    scipy.stats.scoreatpercentile(large_bootstrap_params[:,t==0], 97.5,
-        axis=0)]).T
 
-fig = plt.figure(figsize=(3.42, 4))
-gs = mpl.gridspec.GridSpec(nrows=2, ncols=2,
-        width_ratios=[1,3])
+blind_ax = dict(top=False, bottom=False, left=False, right=False,
+        labelleft=False, labelright=False, labeltop=False,
+        labelbottom=False)
 
-title_ax1 = fig.add_subplot(gs[0,:], frame_on=False)
+fig = plt.figure()
+gs = mpl.gridspec.GridSpec(nrows=1, ncols=5, width_ratios=[1,5,1,1,5]) #have one empty one in the middle for some spacing between plots
+
+### left - location
+title_ax1 = fig.add_subplot(gs[0,:2], frame_on=False)
 title_ax1.tick_params(**blind_ax)
-title_ax1.set_title(r'\textbf{bias effect} ($c$) at $t=0$', size=10)
+title_ax1.set_title(r'\textbf{location}', size=10)
 
+# left - variable names
 text_ax1 = fig.add_subplot(gs[0,0], frame_on=False)
 text_ax1.tick_params(**blind_ax)
 
-text_ax1.text(0.95, 5./6., r'\nth{1} \textalpha', ha='right',
-        va='center', transform=text_ax1.transAxes, size=10)
-text_ax1.text(0.95, 4./6., r'\nth{2} \textalpha', ha='right',
-        va='center', transform=text_ax1.transAxes, size=10)
-text_ax1.text(0.95, 3./6., r'\nth{1} \straighttheta', ha='right',
-        va='center', transform=text_ax1.transAxes, size=10)
-text_ax1.text(0.95, 2./6., r'\nth{2} \straighttheta', ha='right',
-        va='center', transform=text_ax1.transAxes, size=10)
-text_ax1.text(0.95, 1./6., r'trial index', ha='right',
-        va='center', transform=text_ax1.transAxes, size=10)
+for e,d in enumerate(names):
+        text_ax1.text(0.95, float(e+1)/(len(location_dict)+1), d,         ha='right', va='center', transform=text_ax1.transAxes, size=10)
 
-title_ax2 = fig.add_subplot(gs[1,:], frame_on=False)
-title_ax2.tick_params(**blind_ax)
-title_ax2.set_title(r"""\textbf{sensitivity effect} ($d'$) at $t=0$""", size=10)
-
-text_ax2 = fig.add_subplot(gs[1,0], frame_on=False)
-text_ax2.tick_params(**blind_ax)
-
-text_ax2.text(0.95, 5./6., r'\nth{1} \textalpha', ha='right',
-        va='center', transform=text_ax2.transAxes, size=10)
-text_ax2.text(0.95, 4./6., r'\nth{2} \textalpha', ha='right',
-        va='center', transform=text_ax2.transAxes, size=10)
-text_ax2.text(0.95, 3./6., r'\nth{1} \straighttheta', ha='right',
-        va='center', transform=text_ax2.transAxes, size=10)
-text_ax2.text(0.95, 2./6., r'\nth{2} \straighttheta', ha='right',
-        va='center', transform=text_ax2.transAxes, size=10)
-text_ax2.text(0.95, 1./6., r'trial index', ha='right',
-        va='center', transform=text_ax2.transAxes, size=10)
-
+# left - CI
 ci_ax1 = fig.add_subplot(gs[0,1], frame_on=False)
 ci_ax1.tick_params(**dict(left=False, labelleft=False, right=False,
     labelright=False))
@@ -97,17 +122,33 @@ ci_ax1.axvline(0, ls=':', lw=0.75)
 trans1 = mpl.transforms.blended_transform_factory(
     ci_ax1.transData, ci_ax1.transAxes)
 
-for i in xrange(5):
-    if params_p[i]<0.05:
-        color_now=color1
+for i in range(len(params_CI_loc)):
+    if (params_CI_loc[i][1]*params_CI_loc[i][0])>=0:
+        color_now='red'
     else:
         color_now='k'
-    ci_ax1.plot(-params_CI[i], 2*[(5-i)/6.], ls='-', c=color_now,
+    ci_ax1.plot(params_CI_loc[i], 2*[float(i+1)/(len(location_dict)+1)], ls='-', c=color_now,
             transform=trans1)
-    ci_ax1.text(-params_CI[i].mean(), (5-i)/6. + 0.02, r'$p=%.3f$' % params_p[i],
-            transform=trans1, ha='center', va='bottom', size=7, color=color_now)
+## p values take up too much space probably
+#     ci_ax1.text(-params_CI_loc[i].mean(), float(len(location_dict)-i)/len(location_dict) + 0.02, r'$p=%.3f$' % params_p[i],
+#             transform=trans1, ha='center', va='bottom', size=7, color=color_now)
 
-ci_ax2 = fig.add_subplot(gs[1,1], frame_on=False, sharex=ci_ax1)
+
+### right - scale
+title_ax2 = fig.add_subplot(gs[0,3:], frame_on=False)
+title_ax2.tick_params(**blind_ax)
+title_ax2.set_title(r"""\textbf{scale}""", size=10)
+
+# right - variable names
+text_ax2 = fig.add_subplot(gs[0,3], frame_on=False)
+text_ax2.tick_params(**blind_ax)
+
+for e,d in enumerate(names):
+        text_ax2.text(0.95, float(e+1)/(len(scale_dict)+1), d, ha='right', va='center', transform=text_ax2.transAxes,
+        size=10)
+
+# right - CI
+ci_ax2 = fig.add_subplot(gs[0,4], frame_on=False, sharex=ci_ax1)
 ci_ax2.tick_params(**dict(left=False, labelleft=False, right=False,
     labelright=False))
 ci_ax2.plot([0,1], [0,0], 'k-', transform=ci_ax2.transAxes)
@@ -116,32 +157,36 @@ ci_ax2.axvline(0, ls=':', lw=0.75)
 trans2 = mpl.transforms.blended_transform_factory(
     ci_ax2.transData, ci_ax2.transAxes)
 
-for i in xrange(5):
-    if params_p[i+5]<0.05:
-        color_now=color1
+for i in range(len(params_CI_scale)):
+    if (params_CI_scale[i][1]*params_CI_scale[i][0])>=0:
+        color_now='r'
     else:
         color_now='k'
-    ci_ax2.plot(params_CI[i+5], 2*[(5-i)/6.], ls='-', c=color_now,
+    ci_ax2.plot(params_CI_scale[i], 2*[float(i+1)/(len(scale_dict)+1)], ls='-', c=color_now,
             transform=trans2)
-    ci_ax2.text(params_CI[i+5].mean(), (5-i)/6. + 0.02,
-            r'$p=%.3f$' % params_p[i+5],
-            transform=trans2, ha='center', va='bottom', size=7,
-            color=color_now)
+    ## p values take up too much space probably
+    # ci_ax2.text(params_CI_scale[i].mean(), float(len(scale_dict)-i)/len(scale_dict),
+    #         r'$p=%.3f$' % params_p[i],
+    #         transform=trans2, ha='center', va='bottom', size=7,
+    #         color=color_now)
 
-ci_ax1.set_xlim([-np.abs(params_CI[:10]).max(), np.abs(params_CI[:10]).max()])
+ci_ax1.set_xlim([-np.abs(params_CI_loc[:10]).max(), np.abs(params_CI_loc[:10]).max()])
 
+
+# bottom - plot legend
 ci_ax1.set_xlabel('model coefficients')
 ci_ax2.set_xlabel('model coefficients')
 
-ci_ax1.text(0.025, 0.025, r'\textit{more liberal}', ha='left', va='bottom',
+ci_ax1.text(0.025, 0.025, r'\textit{earlier}', ha='left', va='bottom',
         transform=ci_ax1.transAxes, size=7)
-ci_ax1.text(0.975, 0.025, r'\textit{more conservative}', ha='right',
+ci_ax1.text(0.975, 0.025, r'\textit{later}', ha='right',
         va='bottom', transform=ci_ax1.transAxes, size=7)
 
-ci_ax2.text(0.025, 0.025, r'\textit{lower sensitivity}', ha='left', va='bottom',
+ci_ax2.text(0.025, 0.025, r'\textit{consistent}', ha='left', va='bottom',
         transform=ci_ax2.transAxes, size=7)
-ci_ax2.text(0.975, 0.025, r'\textit{higher sensitivity}', ha='right',
+ci_ax2.text(0.975, 0.025, r'\textit{irregular}', ha='right',
         va='bottom', transform=ci_ax2.transAxes, size=7)
 
-gs.tight_layout(fig, pad=0.3)
-fig.savefig('params_sig.pdf')
+#gs.tight_layout(fig, pad=0.3)
+
+fig.savefig(os.path.join(result_folder, 'models/singleVariable/paramsCI.pdf'))
